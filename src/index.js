9d48b45999cafad6bbda834b0480af6d35f21eca
Amir Ali <amirliaqat2020@gmail.com>
2023-06-03 04:22:58 +0500

    Completeing my first challenge project...

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render( 
+  <>
+    <h1>Netflix Series</h1>
+    <p>theses are my 5 favourite series</p>
+    <ol>
+      <li>One</li>
+      <li>Two</li>
+      <li>Three</li>
+      <li>Four</li>
+      <li>Five</li>
+    </ol>
+  </>, 
+  document.getElementById("root")
+);
