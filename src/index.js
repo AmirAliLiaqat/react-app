907e397aeded13d114d060f495298ad06cb147c0
Amir Ali <amirliaqat2020@gmail.com>
2023-06-03 04:43:36 +0500

    using template literals in my code...

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const fname = 'Amir Ali';
+const fname = 'Amir';
+const lname = 'Ali';
 
 ReactDOM.render( 
   <>
-    <h1>my name is {fname}</h1>
-    <p>my nucky number is {Math.random()}</p>
+    <h1>my name is {fname} {lname}</h1>
+    <h1>my name is {fname + lname}</h1>
+    <h1>my name is {fname + " " + lname}</h1>
+    <h1>{`my name is ${fname} ${lname}`}</h1>
   </>, 
   document.getElementById("root")
 );
