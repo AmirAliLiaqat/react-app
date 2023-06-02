af119d5927c9f94b6b8e083926ae219c1ef05ce3
Amir Ali <amirliaqat2020@gmail.com>
2023-06-03 04:33:18 +0500

    writing javascript in side html code...

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const fname = 'Amir Ali';
+
 ReactDOM.render( 
   <>
-    <h1>Netflix Series</h1>
-    <p>theses are my 5 favourite series</p>
-    <ol>
-      <li>One</li>
-      <li>Two</li>
-      <li>Three</li>
-      <li>Four</li>
-      <li>Five</li>
-    </ol>
+    <h1>my name is {fname}</h1>
+    <p>my nucky number is {Math.random()}</p>
   </>, 
   document.getElementById("root")
 );
