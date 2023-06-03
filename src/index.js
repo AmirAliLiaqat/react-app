a0705d4d59c8f1904e4ef88251baa60c2cb111d7
Amir Ali <amirliaqat2020@gmail.com>
2023-06-03 06:34:54 +0500

    Adding internal and inline style...

--- src/index.js
+++ src/index.js
@@ -8,9 +8,17 @@ const img2 = 'https://picsum.photos/310/300';
 const img3 = 'https://picsum.photos/320/300';
 const links = 'http://byte.likesyou.org';
 
+const heading = {
+  color: "#fa9191",
+  textAlign: "center",
+  margin: "50px 0",
+  textTransform: "capitalize",
+  textShadow: "0px 2px 4px #ffe9c5"
+}
+
 ReactDOM.render(
   <>
-    <h1 className='heading'>Hello, My name is {name}</h1>
+    <h1 style={heading}>Hello, My name is {name}</h1>
     <div className='img_div'>
       <img src={img1} alt='randomImages'/>
       <img src={img2} alt='randomImages'/>
