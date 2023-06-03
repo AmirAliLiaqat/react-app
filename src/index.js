9c259125085b376ff660a6e57bdaee0656612edb
Amir Ali <amirliaqat2020@gmail.com>
2023-06-03 05:50:00 +0500

    using JSX attributes...

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const name = 'Amir Ali';
-const currDate = new Date().toLocaleDateString();
-const currTime = new Date().toLocaleTimeString();
+const img1 = 'https://picsum.photos/200/300';
+const img2 = 'https://picsum.photos/250/300';
+const img3 = 'https://picsum.photos/300/400';
+const links = 'http://byte.likesyou.org';
 
 ReactDOM.render(
   <>
-    <h1>Hello, My name is {name}</h1>
-    <p>Todays Date is {currDate}</p>
-    <p>Current Time is {currTime}</p>
+    <h1 contentEditable="true">Hello, My name is {name}</h1>
+    <img src={img1} alt='randomImages'/>
+    <img src={img2} alt='randomImages'/>
+    <a href={links} target='_blank'>
+      <img src={img3} alt='randomImages'/>
+    </a>
   </>,
   document.getElementById('root')
 );
