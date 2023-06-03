75ae3f3f1c41d6c69a1dbffaab7d725c3f6678de
Amir Ali <amirliaqat2020@gmail.com>
2023-06-03 06:13:44 +0500

    Adding random images and style css...

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 const name = 'Amir Ali';
-const img1 = 'https://picsum.photos/200/300';
-const img2 = 'https://picsum.photos/250/300';
-const img3 = 'https://picsum.photos/300/400';
+const img1 = 'https://picsum.photos/300/300';
+const img2 = 'https://picsum.photos/310/300';
+const img3 = 'https://picsum.photos/320/300';
 const links = 'http://byte.likesyou.org';
 
 ReactDOM.render(
   <>
-    <h1 contentEditable="true">Hello, My name is {name}</h1>
-    <img src={img1} alt='randomImages'/>
-    <img src={img2} alt='randomImages'/>
-    <a href={links} target='_blank'>
-      <img src={img3} alt='randomImages'/>
-    </a>
+    <h1 className='heading'>Hello, My name is {name}</h1>
+    <div className='img_div'>
+      <img src={img1} alt='randomImages'/>
+      <img src={img2} alt='randomImages'/>
+      <a href={links} target='_blank'>
+        <img src={img3} alt='randomImages'/>
+      </a>
+    </div>
   </>,
   document.getElementById('root')
 );
