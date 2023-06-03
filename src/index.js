b833cff692c470fb25ca86d6f5ad7eb4a36d9e2e
Amir Ali <amirliaqat2020@gmail.com>
2023-06-03 07:00:43 +0500

    Creating my first mini project...

--- src/index.js
+++ src/index.js
@@ -2,29 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const name = 'Amir Ali';
-const img1 = 'https://picsum.photos/300/300';
-const img2 = 'https://picsum.photos/310/300';
-const img3 = 'https://picsum.photos/320/300';
-const links = 'http://byte.likesyou.org';
+let currDate = new Date(2023,3,6,12);
+currDate = currDate.getHours();
+let greeting = '';
+const cssStyle = {};
 
-const heading = {
-  color: "#fa9191",
-  textAlign: "center",
-  margin: "50px 0",
-  textTransform: "capitalize",
-  textShadow: "0px 2px 4px #ffe9c5"
+if(currDate >= 1 && currDate < 12) {
+  greeting = 'Good Moring';
+  cssStyle.color = 'green';
+} else if(currDate >= 12 && currDate < 19) {
+  greeting = 'Good Afternoon';
+  cssStyle.color = 'orange';
+} else {
+  greeting = 'Good Night';
+  cssStyle.color = 'black';
 }
 
 ReactDOM.render(
   <>
-    <h1 style={heading}>Hello, My name is {name}</h1>
-    <div className='img_div'>
-      <img src={img1} alt='randomImages'/>
-      <img src={img2} alt='randomImages'/>
-      <a href={links} target='_blank'>
-        <img src={img3} alt='randomImages'/>
-      </a>
+    <div>
+      <h1 >Hello Sir, <span style={cssStyle}>{greeting}</span></h1>
     </div>
   </>,
   document.getElementById('root')
