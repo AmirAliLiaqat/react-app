0d65cf36286d25f455e6666ca44ba89a22a60b7e
Amir Ali <amirliaqat2020@gmail.com>
2023-06-06 03:46:16 +0500

    Import and use app.js component in main index file

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
-
-let currDate = new Date(2023,3,6,12);
-currDate = currDate.getHours();
-let greeting = '';
-const cssStyle = {};
-
-if(currDate >= 1 && currDate < 12) {
-  greeting = 'Good Moring';
-  cssStyle.color = 'green';
-} else if(currDate >= 12 && currDate < 19) {
-  greeting = 'Good Afternoon';
-  cssStyle.color = 'orange';
-} else {
-  greeting = 'Good Night';
-  cssStyle.color = 'black';
-}
 
 ReactDOM.render(
   <>
-    <div>
-      <h1 >Hello Sir, <span style={cssStyle}>{greeting}</span></h1>
-    </div>
+    <App />
   </>,
   document.getElementById('root')
 );
