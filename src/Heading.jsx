b2bd90c78737dace60adb9cf08e016087e3a16d5
Amir Ali <amirliaqat2020@gmail.com>
2023-06-06 05:27:02 +0500

    Deleting all of theses components...

--- src/Heading.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Heading() {
-    return(
-        <h1>My name is Amir Ali</h1>
-    );
-}
-
-export default Heading;
--- src/Paragraph.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Paragraph() {
-    return(
-        <p>This is the order list of my university friends...</p>
-    );
-}
-
-export default Paragraph;
