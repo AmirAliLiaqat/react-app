101484362aef201839749699df96d75d5c3ad482
Amir Ali <amirliaqat2020@gmail.com>
2023-06-06 05:11:57 +0500

    Create component for prev mini project...

--- src/App.js
+++ src/App.js
@@ -4,11 +4,27 @@ import Paragraph from "./Paragraph";
 import Order from "./Order";
 
 function App() {
+    let currDate = new Date(2023,3,6,5);
+    currDate = currDate.getHours();
+    let greeting = '';
+    const cssStyle = {};
+
+    if(currDate >= 1 && currDate < 12) {
+    greeting = 'Good Moring';
+    cssStyle.color = 'green';
+    } else if(currDate >= 12 && currDate < 19) {
+    greeting = 'Good Afternoon';
+    cssStyle.color = 'orange';
+    } else {
+    greeting = 'Good Night';
+    cssStyle.color = 'black';
+    }
+    
     return(
         <>
-            <Heading />
-            <Paragraph />
-            <Order />
+            <div>
+                <h1 >Hello Sir, <span style={cssStyle}>{greeting}</span></h1>
+            </div>
         </>
     );
 }
