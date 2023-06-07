1b143bdd34d0cb3b9842e7ca0897b4de79a68726
Amir Ali <amirliaqat2020@gmail.com>
2023-06-07 06:19:08 +0500

    Removing all extra content except basic syntax...

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import React from "react";
-import Card from "./Card";
-import CardData from "./CardData";
-
-const fav_web = 'Ecommerce';
-
-const FavWeb = () => {
-    if(fav_web === 'Ecommerce') {
-        return(
-            <Card key={CardData[1].id} imgsrc={CardData[1].imgsrc} title={CardData[1].title} sname={CardData[1].sname} link={CardData[1].link} />
-        );
-    } else {
-
-    }
-}
 
 function App() {
     return(
         <>
-            <h1>List of top 5 websites in 2023</h1>
-            <div className='cards'>
-                <FavWeb />
-            </div>
         </>
     );
 }
