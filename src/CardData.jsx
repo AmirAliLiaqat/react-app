09e4dcce43317b862d24755f6c42031360ce1376
Amir Ali <amirliaqat2020@gmail.com>
2023-06-07 05:55:36 +0500

    creating component more power full with unique id.

--- src/CardData.jsx
+++ src/CardData.jsx
@@ -1,23 +1,27 @@
 const CardData = [
     {
+        id: 1,
         imgsrc: "https://picsum.photos/seed/picsum/200/300",
         title: "Byte Ki Duniya",
         sname: "Let's Do Code",
         link: "http://byte.likesyou.org"
     },
     {
+        id: 2,
         imgsrc: "https://picsum.photos/id/237/200/300",
         title: "Mehar Herbals",
         sname: "Heart of perfect health",
         link: "http://meharherbals.likesyou.org"
     },
     {
+        id: 3,
         imgsrc: "https://picsum.photos/200/300?grayscale",
         title: "ByteBunch",
         sname: "IT Solutions",
         link: "https://bytebunch.com"
     },
     {
+        id: 4,
         imgsrc: "https://picsum.photos/200/300.jpg",
         title: "ByteBunch Blog",
         sname: "Another Wordpress Site",
