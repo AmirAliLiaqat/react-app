df6c93b95f6f320bbfb268e92cd71f08150ad780
Amir Ali <amirliaqat2020@gmail.com>
2023-06-08 07:16:46 +0500

    Removing all extra code...

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 
 const App = () => {
-    let currTime = new Date().toLocaleTimeString();
-
-    const [newTime, setTime] = useState(currTime);
-
-    const updateTime = () => {
-        let newUpdateTime = new Date().toLocaleTimeString();
-        setTime(newUpdateTime);
-    }
-    setInterval(updateTime,1000);
     return(
         <>
-            <h1>{newTime}</h1>
         </>
     );
 }
