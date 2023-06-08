062c638fa7633b227cd5a660aeec75f62b77982c
Amir Ali <amirliaqat2020@gmail.com>
2023-06-08 08:16:56 +0500

    Create button events with event handling...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+    const purple = '#8e44ad';
+    const [bg, setBg] = useState(purple);
+    const [name, setName] = useState('Click Me');
+
+    const bgChange = () => {
+        let newBg = '#34495e';
+        setBg(newBg);
+        setName('Clicked 👍');
+    }
+    const bgBack = () => {
+        setBg(purple);
+        setName('Double Clicked 👍👍');
+    }
+
     return(
         <>
+            <div style={{backgroundColor: bg}}>
+                <button onMouseEnter={bgChange} onMouseLeave={bgBack}>{name}</button>
+            </div>
         </>
     );
 }
