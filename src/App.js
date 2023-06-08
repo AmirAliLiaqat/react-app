306d1ca990a931d54da8e0d3539675ad950108aa
Amir Ali <amirliaqat2020@gmail.com>
2023-06-09 02:39:13 +0500

    Creating form with four input fields...

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React, { useState } from "react";
 
 const App = () => {
-    const [name, setName] = useState();
+    const [name, setName] = useState({
+        fname: '',
+        lname: '',
+        email: '',
+        phone: '',
+    });
+
+    const inputEvent = (event) => {
+        // const value = event.target.value;
+        // const name = event.target.name;
+
+        const {value,name} = event.target;
+
+        setName((preValue) => {
+            // console.log(preValue);
+            if(name === 'fname') {
+                return{
+                    fname: value,
+                    lname: preValue.lname,
+                    email: preValue.email,
+                    phone: preValue.phone,
+                }
+            } else if(name === 'lname') {
+                return{
+                    fname: preValue.fname,
+                    lname: value,
+                    email: preValue.email,
+                    phone: preValue.phone,
+                }
+            } else if(name === 'email') {
+                return{
+                    fname: preValue.fname,
+                    lname: preValue.lname,
+                    email: value,
+                    phone: preValue.phone,
+                }
+            } else if(name === 'phone') {
+                return{
+                    fname: preValue.fname,
+                    lname: preValue.lname,
+                    email: preValue.email,
+                    phone: value,
+                }
+            }
+        });
+    }
 
     const onSubmit = (e) => {
         e.preventDefault();
-    }
-
-    const inputEvent = (event) => {
-        setName(event.target.value);
+        alert('form submitted');
     }
 
     return(
@@ -16,9 +58,13 @@ const App = () => {
             <div className="main_div">
                 <form onSubmit={onSubmit}>
                     <div>
-                        <h1>Hello {fullName} {newLastName}</h1>
-                        <input type="text" onChange={inputEvent} value={name} placeholder="Enter Your First Name"></input>
-                        <input type="text" onChange={inputEvent} value={lastName} placeholder="Enter Your Last Name"></input>
+                        <h1>Hello {name.fname} {name.lname}</h1>
+                        <p>{name.email}</p>
+                        <p>{name.phone}</p>
+                        <input type="text" name='fname' onChange={inputEvent} value={name.fname} placeholder="Enter Your First Name"></input>
+                        <input type="text" name='lname' onChange={inputEvent} value={name.lname} placeholder="Enter Your Last Name"></input>
+                        <input type="email" name='email' onChange={inputEvent} value={name.email} placeholder="Enter Your Email" autoComplete="off"></input>
+                        <input type="number" name='phone' onChange={inputEvent} value={name.phone} placeholder="Enter Your Phone"></input>
                         <button type="submit">Submit</button>
                     </div>
                 </form>
