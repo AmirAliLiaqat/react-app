205e86fd490f7260861bdd3be12741088ad59302
Amir Ali <amirliaqat2020@gmail.com>
2023-06-08 08:44:36 +0500

    Creating form in react with states and hooks...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
 
 const App = () => {
+    const [name, setName] = useState();
+    const [fullName, setFullName] = useState();
+    const inputEvent = (event) => {
+        // console.log();
+        setName(event.target.value)
+    }
+    const changeText = () => {
+        setFullName(name);
+    }
 
     return(
         <>
             <div>
-                <button></button>
+                <h1>Hello {fullName}</h1>
+                <input type="text" onChange={inputEvent} value={name} placeholder="Enter Your Name"></input>
+                <button onClick={changeText}>Submit</button>
             </div>
         </>
     );
