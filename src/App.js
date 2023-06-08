ac84c9e83628ee33549115671db2ae543ad1868f
Amir Ali <amirliaqat2020@gmail.com>
2023-06-08 09:23:49 +0500

    Removing extra code...

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import React, { useState } from "react";
 
 const App = () => {
     const [name, setName] = useState();
-    const [lastName, setLastlName] = useState();
-    const [fullName, setFullName] = useState();
-    const [newLastName, setNewLastlName] = useState();
-
-    const inputEvent = (event) => {
-        setName(event.target.value);
-    }
-
-    const inputEventTwo = (event) => {
-        setLastlName(event.target.value);
-    }
 
     const onSubmit = (e) => {
         e.preventDefault();
-        setFullName(name);
-        setNewLastlName(lastName);
+    }
+
+    const inputEvent = (event) => {
+        setName(event.target.value);
     }
 
     return(
@@ -27,7 +18,7 @@ const App = () => {
                     <div>
                         <h1>Hello {fullName} {newLastName}</h1>
                         <input type="text" onChange={inputEvent} value={name} placeholder="Enter Your First Name"></input>
-                        <input type="text" onChange={inputEventTwo} value={lastName} placeholder="Enter Your Last Name"></input>
+                        <input type="text" onChange={inputEvent} value={lastName} placeholder="Enter Your Last Name"></input>
                         <button type="submit">Submit</button>
                     </div>
                 </form>
