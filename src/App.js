8a6a5e02f1c4c0824c98fabad1c7c30ae90e8756
Amir Ali <amirliaqat2020@gmail.com>
2023-06-08 06:46:58 +0500

    Create counter project with useState hook...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+    const state = useState();
+
+    const [count, setCount] = useState(0);
+
+    const IncNum = () => {
+        setCount(count + 1);
+    }
     return(
         <>
+            <h1>{count}</h1>
+            <button onClick={IncNum}>Click Me</button>
         </>
     );
 }
