50a76f516619d3f69dfe208997c998c01eb84bd5
Amir Ali <amirliaqat2020@gmail.com>
2023-06-09 06:54:16 +0500

    Removing all extra code...

--- src/App.js
+++ src/App.js
@@ -1,47 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 
 const App = () => {
-    const [name, setName] = useState({
-        fname: '',
-        lname: '',
-        email: '',
-        phone: '',
-    });
-
-    const inputEvent = (event) => {
-        const {value,name} = event.target;
-
-        setName((preValue) => {
-            console.log(preValue);
-
-            return{
-                ...preValue,
-                [name]: value
-            }
-        });
-    }
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-        alert('form submitted');
-    }
 
     return(
         <>
-            <div className="main_div">
-                <form onSubmit={onSubmit}>
-                    <div>
-                        <h1>Hello {name.fname} {name.lname}</h1>
-                        <p>{name.email}</p>
-                        <p>{name.phone}</p>
-                        <input type="text" name='fname' onChange={inputEvent} value={name.fname} placeholder="Enter Your First Name"></input>
-                        <input type="text" name='lname' onChange={inputEvent} value={name.lname} placeholder="Enter Your Last Name"></input>
-                        <input type="email" name='email' onChange={inputEvent} value={name.email} placeholder="Enter Your Email" autoComplete="off"></input>
-                        <input type="number" name='phone' onChange={inputEvent} value={name.phone} placeholder="Enter Your Phone"></input>
-                        <button type="submit">Submit</button>
-                    </div>
-                </form>
-            </div>
         </>
     );
 }
