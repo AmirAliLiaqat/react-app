03e7a056f3a0a5d0e4307462d637bef82f8a8db4
Amir Ali <amirliaqat2020@gmail.com>
2023-06-09 08:36:47 +0500

    Creating todo list project...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import ToDoList from "./ToDoList";
 
 const App = () => {
+    const [inputList, setInputList] = useState('');
+    const [Items, setItems] = useState([]);
+
+    const itemEvent = (event) => {
+        setInputList(event.target.value);
+    }
+
+    const listOfItems = () => {
+        setInputList("");
+        setItems((oldItems) => {
+            return[...oldItems, inputList];
+        });
+    }
+
+    const deleteItems = (id) => {
+        setItems((oldItems) => {
+            return oldItems.filter((arrElem, index) => {
+                return index !== id;
+            });
+        });
+    }
 
     return(
         <>
+            <div className="main_div">
+                <div className="center_div">
+                    <br/>
+                    <h1>ToDo List</h1>
+                    <br/>
+                    <input type="text" placeholder="Add a Items" value={inputList} onChange={itemEvent} />
+                    <button className="newBtn" onClick={listOfItems}>+</button>
+
+                    <ol>
+                        {
+                            Items.map((itemval, index) => {
+                                return <ToDoList key={index} id={index} text={itemval} onSelect={deleteItems} />
+                            })
+                        }
+                    </ol>
+                </div>
+            </div>
         </>
     );
 }
