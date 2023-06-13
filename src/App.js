235933cda2baec0672d67dbb647c705df3678d0b
Amir Ali <amirliaqat2020@gmail.com>
2023-06-13 09:55:49 +0500

    Removing all extra code from both files...

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 
 const App = () => {
-    const [num, setNum] = useState(0);
-
-    const incNum = () => {
-        setNum(num + 1);
-    }
-
-    const decNum = () => {
-        if(num > 0) {
-            setNum(num - 1);
-        } else {
-            alert('Soory, Zero Limit Reached');
-            setNum(0);
-        }
-    }
-
     return(
         <>
-            <div className="main_div">
-                <div className="center_div">
-                    <h1>{num}</h1>
-                    <div className="btn_div">
-                        <button onClick={incNum}>Increm</button>
-                        <button onClick={decNum}>Decrem</button>
-                    </div>
-                </div>
-            </div>
         </>
     );
 }
