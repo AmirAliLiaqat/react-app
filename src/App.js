5a28d815a50d2f478604009860c6af955f73935b
Amir Ali <amirliaqat2020@gmail.com>
2023-06-13 07:59:15 +0500

    Create increment and decrement project...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { useState } from "react";
 
 const App = () => {
+    const [num, setNum] = useState(0);
+
+    const incNum = () => {
+        setNum(num + 1);
+    }
+
+    const decNum = () => {
+        if(num > 0) {
+            setNum(num - 1);
+        } else {
+            alert('Soory, Zero Limit Reached');
+            setNum(0);
+        }
+    }
+
     return(
         <>
             <div className="main_div">
                 <div className="center_div">
-                    <br/>
-                    <h1>ToDo List</h1>
+                    <h1>{num}</h1>
+                    <div className="btn_div">
+                        <button onClick={incNum}>Increm</button>
+                        <button onClick={decNum}>Decrem</button>
+                    </div>
                 </div>
             </div>
         </>
