f1c18f23b840b73af0725d8582be2e2dc501a668
Amir Ali <amirliaqat2020@gmail.com>
2023-06-14 07:30:55 +0500

    Adding newly created components in main file...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Header from "./Header";
+import CreateNode from "./CreateNode";
+import Footer from "./Footer";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
     return(
         <>
+            <Header />
+            <CreateNode />
+            <Footer />
         </>
     );
 }
