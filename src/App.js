37fb9c086734d4f671a874727e65be9afc817958
Amir Ali <amirliaqat2020@gmail.com>
2023-06-14 07:26:30 +0500

    Creating cards gallery with bootstrap...

--- src/App.js
+++ src/App.js
@@ -4,7 +4,41 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 const App = () => {
     return(
         <>
-            <h1 className="text-danger text-center">Hello, Welcome to this site...</h1>
+            <h1 className="text-danger text-center my-5">Hello, Welcome to this site...</h1>
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col">
+                        <div className="card">
+                            <img src="https://picsum.photos/seed/picsum/200/300" className="card-img-top" height="200" />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <img src="https://picsum.photos/seed/picsum/200/300" className="card-img-top" height="200" />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <img src="https://picsum.photos/seed/picsum/200/300" className="card-img-top" height="200" />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
