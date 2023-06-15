76b5a325caf19b9636ff84259415d80b42f900ad
Amir Ali <amirliaqat2020@gmail.com>
2023-06-15 08:52:20 +0500

    Creating the header content and style it...

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
     return(
         <>
-
+            <div className="container-fluid bg-warning text-white p-4">
+                <h3>Byte Keep</h3>
+            </div>
         </>
     );
 }
