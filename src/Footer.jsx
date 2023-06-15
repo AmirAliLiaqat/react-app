9a7250eb298386483f91e5a43d6288e3bb5bfb46
Amir Ali <amirliaqat2020@gmail.com>
2023-06-15 08:52:37 +0500

    Creating the footer content and style it...

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 const Footer = () => {
+    const currDate = new Date().getFullYear();
     return(
         <>
-
+            <div className="container-fluid bg-white text-white text-center p-3">
+                <p>Copyright © {currDate}</p>
+            </div>
         </>
     );
 }
