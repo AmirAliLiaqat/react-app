b2cfae91dd622da069d4c2817f97e117ee1a5051
Amir Ali <amirliaqat2020@gmail.com>
2023-06-17 02:57:49 +0500

    Creating hierarcal components and fetch data...

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
-import React, { useState } from "react";
-import Header from "./Header";
-import CreateNote from "./CreateNote";
-import Note from "./Note";
-import Footer from "./Footer";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import CompA from "./CompA";
 
 const App = () => {
-    const [addItem, setAddItem] = useState([]);
-
-    const addNote = (note) => {
-        setAddItem((prevData) => {
-            return[...prevData, note];
-        })
-    }
-
-    const onDelete = (id) => {
-        setAddItem((oldData) =>
-            oldData.filter((currData, index) => {
-                return index !== id;
-            })
-        )
-    }
-
-    return(
-        <>
-            <Header />
-            <CreateNote passNote={addNote} />
-            <div className="container-fluid py-5">
-                {
-                    addItem.map((val, index) => {
-                        return <Note key={index} id={index} title={val.title} content={val.content} deleteItem={onDelete} />;
-                    })
-                }
-            </div>
-            <Footer />
-        </>
-    );
+    return <CompA />;
 }
 
 export default App;
---
+++ src/CompC.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CompC = () => {
+    return <h1>This is my component...</h1>;
+}
+
+export default CompC;
