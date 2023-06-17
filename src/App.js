7290ddfb18bb56017069dcb46ab7e47ebc7f970c
Amir Ali <amirliaqat2020@gmail.com>
2023-06-17 09:10:21 +0500

    Removing extra code...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Statewise from "./Statewise/Statewise";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
     return(
         <>
-            <h1>Pakistan COVID-19 Dashoard</h1>
         </>
     );
 }
