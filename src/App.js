f6b78bd9a33590a40265b90845671aa05ad2f433
Amir Ali <amirliaqat2020@gmail.com>
2023-06-18 01:47:36 +0500

    Importing all pages and routing it...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from './About';
+import Contact from './Contact';
+import Error from './Error';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
     return(
         <>
-            <h1 className="text-center my-5">Hi, this is my react app...</h1>
+            <Routes>
+                <Route path="/" Component={Home}/>
+                <Route path="/about" Component={About}/>
+                <Route path="/contact" Component={Contact}/>
+                <Route path="*" Component={Error} />
+            </Routes>
         </>
     );
 }
