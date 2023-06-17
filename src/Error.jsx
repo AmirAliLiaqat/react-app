58acf827022c1d2c40dee18951bcb32c07eba7d1
Amir Ali <amirliaqat2020@gmail.com>
2023-06-18 01:46:48 +0500

    Creating new components...

---
+++ src/Error.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = () => {
+    return(
+        <>
+            <h1 className="text-center my-5">OOps! Page Not Found!</h1>
+        </>
+    )
+}
+
+export default Error;
