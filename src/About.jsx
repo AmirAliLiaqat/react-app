61929e4efef6da13204a42abb17592da9f731f7b
Amir Ali <amirliaqat2020@gmail.com>
2023-06-18 01:47:12 +0500

    Writing the title of these pages...

--- src/About.jsx
+++ src/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
     return(
         <>
-
+            <h1 className="text-center my-5">Hi, i am a About page...</h1>
         </>
     )
 }
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Contact = () => {
     return(
         <>
-
+            <h1 className="text-center my-5">Hi, i am a Contact page...</h1>
         </>
     )
 }
