ec40c49f2e6b87c594aa7d601819096860f8b499
Amir Ali <amirliaqat2020@gmail.com>
2023-06-17 08:16:08 +0500

    Changing the title of document with useEffect hook

--- src/App.js
+++ src/App.js
@@ -2,25 +2,18 @@ import React, { useEffect, useState } from "react";
 
 const App = () => {
     const [num, setNum] = useState(0);
-    const [nums, setNums] = useState(0);
 
     const incNum = () => {
         setNum(num + 1);
     }
 
-    const incNums = () => {
-        setNums(nums + 1);
-    }
-
     useEffect(() => {
-        alert('i am clicked');
-    }, [num])
+        document.title=`You click me ${num} times`;
+    })
 
     return(
         <>
-            <button onClick={incNum}>click me {num}</button>
-            <br/>
-            <button onClick={incNums}>click me {nums}</button>
+            <button onClick={incNum} style={{margin: 20}}>click me {num}</button>
         </>
     );
 }
