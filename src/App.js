7ac462a1f33032b8f2ddc5f2bfce622ed11059e7
Amir Ali <amirliaqat2020@gmail.com>
2023-06-17 08:22:30 +0500

    Removing extra code...

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const App = () => {
-    const [num, setNum] = useState(0);
-
-    const incNum = () => {
-        setNum(num + 1);
-    }
-
-    useEffect(() => {
-        document.title=`You click me ${num} times`;
-    })
 
     return(
         <>
-            <button onClick={incNum} style={{margin: 20}}>click me {num}</button>
         </>
     );
 }
