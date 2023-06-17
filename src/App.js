c67a23c9f77c8f53c314e3f175a3ca6b5e557e43
Amir Ali <amirliaqat2020@gmail.com>
2023-06-17 08:08:30 +0500

    Creating two buttons with useEffect hook...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+    const [num, setNum] = useState(0);
+    const [nums, setNums] = useState(0);
+
+    const incNum = () => {
+        setNum(num + 1);
+    }
+
+    const incNums = () => {
+        setNums(nums + 1);
+    }
+
+    useEffect(() => {
+        alert('i am clicked');
+    }, [num])
+
     return(
         <>
+            <button onClick={incNum}>click me {num}</button>
+            <br/>
+            <button onClick={incNums}>click me {nums}</button>
         </>
     );
 }
