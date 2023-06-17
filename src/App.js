298782f6bf85d395d8470d97f95b69883220e764
Amir Ali <amirliaqat2020@gmail.com>
2023-06-18 01:14:23 +0500

    Adding route and switch modules...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
     return(
         <>
+            <h1 className="text-center my-5">Hi, this is my react app...</h1>
         </>
     );
 }
