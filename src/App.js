dfca89d27df8df71ff943f9b16bcefc31db3e1d9
Amir Ali <amirliaqat2020@gmail.com>
2023-06-17 07:15:05 +0500

    Printing my full name with react context API...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { createContext } from "react";
 import CompA from "./CompA";
 
+const FirstName = createContext();
+const LastName = createContext();
+
 const App = () => {
-    return <CompA />;
+    return(
+        <>
+            <FirstName.Provider value={"Amir"}>
+                <LastName.Provider value={"Ali"}>
+                    <CompA />
+                </LastName.Provider>
+            </FirstName.Provider>
+        </>
+    );
 }
 
 export default App;
+export { FirstName, LastName };
--- src/CompC.jsx
+++ src/CompC.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { FirstName, LastName } from "./App";
 
 const CompC = () => {
-    return <h1>This is my component...</h1>;
+    return(
+        <>
+            <FirstName.Consumer>
+                {(fname) => {
+                    return(
+                        <LastName.Consumer>
+                            {(lname) => {
+                                return(
+                                    <h1>
+                                        My name is {fname} {lname}
+                                    </h1>
+                                );
+                            }}
+                        </LastName.Consumer>
+                    );
+                }}
+            </FirstName.Consumer>
+        </>
+    );
 }
 
 export default CompC;
