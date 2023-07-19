4139fdbb757af64a9d61f7872b824d9706a47f43
Amir Ali <amirliaqat2020@gmail.com>
2023-07-20 04:26:41 +0500

    Styling the navbar items...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,36 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+    const location = useLocation();
+    const uniqueClass = location.pathname === `/` ? "text-white" : "";
+    
     return(
         <>
             <nav className="custom-navbar" data-spy="affix" data-offset-top="20">
                 <div className="container">
-                    <a className="logo" href="/">Meyawo</a>         
+                    <h1><NavLink to='/' className={ uniqueClass }>Meyawo</NavLink></h1>
                     <ul className="list-unstyled d-flex ml-auto">
                         <li className="item mx-3">
-                            <NavLink to='/'>Home</NavLink>
+                            <NavLink to='/' className={ uniqueClass }>Home</NavLink>
                         </li>
                         <li className="item mx-3">
-                            <NavLink to='/about'>About</NavLink>
+                            <NavLink to='/about' className={ uniqueClass }>About</NavLink>
                         </li>
                         <li className="item mx-3">
-                            <NavLink to='/services'>Services</NavLink>
+                            <NavLink to='/services' className={ uniqueClass }>Services</NavLink>
                         </li>
                         <li className="item mx-3">
-                            <NavLink to='/portfolio'>Portfolio</NavLink>
+                            <NavLink to='/portfolio' className={ uniqueClass }>Portfolio</NavLink>
                         </li>
                         <li className="item mx-3">
-                            <NavLink to='/blog'>Blog</NavLink>
+                            <NavLink to='/blog' className={ uniqueClass }>Blog</NavLink>
                         </li>
                         <li className="item mx-3">
-                            <NavLink to='/contact'>Contact</NavLink>
+                            <NavLink to='/contact' className={ uniqueClass }>Contact</NavLink>
                         </li>
                         <li className="item mx-3">
-                            <NavLink to='/search'>🔍</NavLink>
+                            <NavLink to='/search' className={ uniqueClass }><i class="fa-solid fa-magnifying-glass"></i></NavLink>
                         </li>
                     </ul>
                     {/* <a href="javascript:void(0)" id="nav-toggle" className="hamburger hamburger--elastic">
